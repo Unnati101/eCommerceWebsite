324d8c707d06115c12ba66982649564620c14292
Unnati Rastogi <rastogiunnati25@gmail.com>
2023-12-01 23:05:30 +0530

    created routing

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ import colors from 'colors';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
-
+import authRoutes from './routes/authRoutes.js'
+import cors from 'cors';
 //configuring env
 dotenv.config();
 
@@ -17,6 +18,11 @@ const app = express();
 //middlewares
 app.use(express.json())
 app.use(morgan('dev'))
+
+app.use(cors())
+//routes
+app.use("api/v1/auth", authRoutes);
+
 //rest api
 app.get("/",(req, res) =>{
    res.send("<h1>welcome to ecommerce app</h1>");
