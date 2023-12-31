15065f01e8768b3addd952c99e80ad86ac3c40a2
Unnati Rastogi <rastogiunnati25@gmail.com>
2023-12-31 15:50:26 +0530

    added register page

---
+++ client/src/pages/Auth/Register.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import Layout from '../../components/Layout/Layout'
+
+const Register = () => {
+  return (
+   <Layout title="Register- Ecommerce App">
+   <div className="register">
+    <h1>Register page</h1>
+    <form>
+  <div className="mb-3">
+    <input 
+    type="text" 
+    className="form-control"
+     id="exampleInputEmail1"
+     placeholder="Enter your name" 
+     />
+    
+  </div>
+  <div className="mb-3">
+    <input 
+    type="email" 
+    className="form-control"
+     id="exampleInputEmail1" 
+     placeholder="Enter your email"
+
+     />
+    
+  </div>
+  <div className="mb-3">
+    <input 
+    type="password" 
+    className="form-control" 
+    id="exampleInputPassword1"
+    placeholder="Enter your password"
+    />
+  </div>
+  <div className="mb-3">
+    <input 
+    type="text" 
+    className="form-control"
+     id="exampleInputEmail1" 
+     placeholder="Enter your phone"
+     />
+    
+  </div>
+  <div className="mb-3">
+    <input 
+    type="text" 
+    className="form-control"
+     id="exampleInputEmail1" 
+     placeholder="Enter your address"
+     />
+    
+  </div>
+ 
+  <button type="submit" class="btn btn-primary">
+    Submit
+</button>
+</form>
+</div>
+</Layout>
+  );
+};
+
+export default Register;
