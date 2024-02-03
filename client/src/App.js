dc31b5754dcfc00a4ed90165f7d0b2cfe6ccb669
RitvikSh <praveensiarma@gmail.com>
2024-02-03 18:50:11 +0530

    header fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,52 @@
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Homepage from './pages/Homepage';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Policy from './pages/Policy';
 import PageNotFound from './pages/PageNotFound';
-import Register from './pages/Auth/Register'; 
-import  Login  from './pages/Auth/Login';
+import Register from './pages/Auth/Register';
+import Login from './pages/Auth/Login';
 import Dashboard from './pages/user/Dashboard.js';
 import PrivateRoute from './components/Routes/PrivateRoute';
 import ForgotPassword from './pages/Auth/ForgotPassword';
 import AdminRoute from './components/Routes/AdminRoute';
-import  AdminDashboard from './pages/Admin/AdminDashboard';
+import AdminDashboard from './pages/Admin/AdminDashboard';
 import CreateCategory from './pages/Admin/CreateCategory';
 import CreateProduct from './pages/Admin/CreateProduct';
 import Users from './pages/Admin/Users';
 import Orders from './pages/user/Orders.js';
 import Profile from './pages/user/Profile.js';
 import Products from './pages/Admin/Products.js';
-import  UpdateProduct  from './pages/Admin/UpdateProduct.js';
+import UpdateProduct from './pages/Admin/UpdateProduct.js';
 function App() {
   return (
     <>
-    <Routes>  
-      <Route path = "/" element={<Homepage/>} />
-      <Route path= "/dashboard" element={<PrivateRoute/>}>
-      <Route path= "/dashboard/user" element={<Dashboard/>}/>
-      <Route path= "user/orders" element={<Orders/>}/>
-      <Route path= "user/profile" element={<Profile/>}/>
-      </Route>
-      <Route path= "/dashboard" element={<AdminRoute/>}> 
-      <Route path= "admin" element={<AdminDashboard />} />
-      <Route path= "admin/create-category" element={<CreateCategory/>} />
-      <Route path= "admin/create-product" element={<CreateProduct/>} />
-      <Route path= "admin/product/:slug" element={<UpdateProduct/>} />
-      <Route path= "admin/products" element={<Products/>} />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/dashboard" element={<PrivateRoute />}>
+          <Route path="/dashboard/user" element={<Dashboard />} />
+          <Route path="user/orders" element={<Orders />} />
+          <Route path="user/profile" element={<Profile />} />
+        </Route>
+        <Route path="/dashboard" element={<AdminRoute />}>
+          <Route path="admin" element={<AdminDashboard />} />
+          <Route path="admin/create-category" element={<CreateCategory />} />
+          <Route path="admin/create-product" element={<CreateProduct />} />
+          <Route path="admin/product/:slug" element={<UpdateProduct />} />
+          <Route path="admin/products" element={<Products />} />
 
-      <Route path= "admin/users" element={<Users/>} />
-      </Route>
+          <Route path="admin/users" element={<Users />} />
+        </Route>
 
-      <Route path= "/register" element={<Register/>}/>
-      <Route path= "/forgot-password" element={<ForgotPassword/>}/>
-      <Route path= "/dashboard" element={<Dashboard/>}/>
-      <Route path= "/login" element={<Login/>}/>
-      <Route path= "/about" element={<About/>}/>
-      <Route path = "/contact"element={<Contact/>}/>
-      <Route path ="/policy" element={<Policy/>}/>
-      <Route path = "*" element={<PageNotFound/>}/>
-    </Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </>
   );
 }
